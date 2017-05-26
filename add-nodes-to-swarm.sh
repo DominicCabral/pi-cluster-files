f4b4e72f774e96a34931f104afe51bbd1f64f6a8
Dominic Cabral <dcabral@netra.io>
2017-05-26 11:56:27 -0400

    Update add-nodes-to-swarm.sh

--- add-nodes-to-swarm.sh
+++ add-nodes-to-swarm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SLAVE_NODE_IPS='10.0.0.206 10.0.0.13 10.0.0.224'
+SLAVE_NODE_IPS='LIST OF NODE IPS'
 
 # Install Parallel SSH
 sudo apt-get install -y pdsh
@@ -9,4 +9,4 @@ sudo apt-get install -y pdsh
 echo "alias parallel_do='pdsh -w - -R exec ssh -i .ssh/id_rsa.pem -o StrictHostKeyChecking=no %h'" >> ~/.bash_aliases
 source ~/.bash_aliases
 
-echo $SLAVE_NODE_IPS | parallel_do 'docker swarm join --token SWMTKN-1-62ncaue3329p4keevoa3ldyuim6usudqeqfl7d6fvyy4pgzyd5-6o6s61oxcfx756mqboraoh3zc 10.0.0.181:2377'
+echo $SLAVE_NODE_IPS | parallel_do 'PASTE JOIN COMMAND'
